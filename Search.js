a171fda95886619f1a35598a2e1c30f8a8ef5d56
SELVIN R <steelselvin2002@gmail.com>
2025-11-13 21:50:28 +0530

    first commite

---
+++ Search.js
@@ -0,0 +1,61 @@
+const acceskey ="nPi3rHD_dYV9mfyVFFKKuLZKXx9QDo6NyXwgSdExqKM";
+
+const formEl = document.querySelector("form");
+const searchInputEl = document.getElementById("search-input");
+const searchResultEl = document.querySelector(".search-results");
+const showMoreButton = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImges() {
+
+    inputData = searchInputEl.value;
+
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${acceskey}`;
+    console.log("Fetching:", url);
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (page === 1) {
+        searchResultEl.innerHTML = "";
+    }
+
+    const results = data.results;
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+
+        const image = document.createElement("img");
+        image.src = result.urls.small;     // FIXED
+        image.alt = result.alt_description;
+
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description || "View Image";
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+
+        searchResultEl.appendChild(imageWrapper);
+    });
+
+    page++;
+
+    if (page > 1) {
+        showMoreButton.style.display = "block";
+    }
+}
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImges();
+});
+
+showMoreButton.addEventListener("click", () => {
+    searchImges();
+});
